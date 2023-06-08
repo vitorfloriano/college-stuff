2286866895100bff214a0e3fe6d52f311c1ad8d5
Vitor Floriano <107767584+vitorfloriano@users.noreply.github.com>
2023-06-07 21:12:35 -0300

    Just practicing a bit.

--- import json.py
+++ import json.py
@@ -2,15 +2,19 @@ import json
 
 d = dict()
 
+#Creates a dictionary
+d = {}
+
+#Adds key-value pairs to the dictionary
 d['name'] = 'Bob'
 d['age'] = 20
 d['score'] = 88
 
+#Converts the dictionary into a JSON string
 jd = json.dumps(d)
 
+#Prints the JSON string
 print(jd)
-
-dd = json.loads(jd)
 
 print(dd)
 
